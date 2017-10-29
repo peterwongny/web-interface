bdecb2cbd3baeab8d146440976878ce7054a0cc3
Peter Wong <impeterwong@hotmail.com>
2017-10-29 21:18:21 +0800

    'setup'

---
+++ src/main/java/com/web/webpage/WebpageApplication.java
@@ -0,0 +1,12 @@
+package com.web.webpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebpageApplication.class, args);
+	}
+}
