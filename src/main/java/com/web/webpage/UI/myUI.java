1a3d14c939cd7366bd169312fced758006b6cee7
Peter Wong <impeterwong@hotmail.com>
2017-10-31 20:32:36 +0800

    setup menu and login page(Wrong format)

--- src/main/java/com/web/webpage/UI/myUI.java
+++ src/main/java/com/web/webpage/UI/myUI.java
@@ -1,35 +1,76 @@
 package com.web.webpage.UI;
 
+import javax.servlet.annotation.WebServlet;
+
 import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.Responsive;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.themes.ValoTheme;
+import com.web.webpage.authentication.AccessControl;
+import com.web.webpage.authentication.BasicAccessControl;
+import com.web.webpage.authentication.LoginScreen;
 import com.web.webpage.database.Tour;
 import com.web.webpage.database.TourRepository;
 
 
 @SpringUI
 @Theme("valo")
-public class myUI extends UI{
-	
-	private final TourRepository tourRepo;
-	
-	final Grid<Tour> grid;
-	
-	public myUI(TourRepository tourRepo) {
+/**
+ * Main UI class of the application that shows either the login screen or the
+ * main view of the application depending on whether a user is signed in.
+ *
+ * The @Viewport annotation configures the viewport meta tags appropriately on
+ * mobile devices. Instead of device based scaling (default), using responsive
+ * layouts.
+ */
+public class myUI extends UI {
+
+    private AccessControl accessControl = new BasicAccessControl();
+    
+    public final TourRepository tourRepo;
+    
+    public myUI(TourRepository tourRepo) {
 		this.tourRepo = tourRepo;
-		this.grid = new Grid<>(Tour.class);
 	}
-	
-	@Override
-    protected void init(VaadinRequest request) {
-		setContent(grid);
-		listTours();
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        Responsive.makeResponsive(this);
+        setLocale(vaadinRequest.getLocale());
+        getPage().setTitle("TourCompany");
+        if (!accessControl.isUserSignedIn()) {
+        	
+        	LoginScreen loginScreen = new LoginScreen(accessControl);
+        	loginScreen.addEventListener(e -> showMainView());
+        	
+        	setContent(loginScreen);
+        	
+        } else {
+            showMainView();
+        }
     }
-	
-	private void listTours() {
-	    grid.setItems(tourRepo.findAll());
-	    grid.setColumnOrder("id", "name", "description", "duration", "day", "weekday_price", "weekend_price", "hits");
-	}
+
+    protected void showMainView() {
+        addStyleName(ValoTheme.UI_WITH_MENU);
+        setContent(new MainScreen(myUI.this));
+        getNavigator().navigateTo(getNavigator().getState());
+    }
+
+    public static myUI get() {
+        return (myUI) UI.getCurrent();
+    }
+
+    public AccessControl getAccessControl() {
+        return accessControl;
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MockAppUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = myUI.class, productionMode = false)
+    public static class myUIServlet extends VaadinServlet {
+    }
 }
