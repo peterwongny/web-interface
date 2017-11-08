f3d5cf02e09834226bb6ca23b7fb1b28b2bad1f2
Peter Wong <impeterwong@hotmail.com>
2017-11-08 22:07:52 +0800

    change to production mode

--- src/main/java/com/web/webpage/UI/myUI.java
+++ src/main/java/com/web/webpage/UI/myUI.java
@@ -91,7 +91,7 @@ public class myUI extends UI {
     }
 
     @WebServlet(urlPatterns = "/*", name = "myUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = myUI.class, productionMode = false)
+    @VaadinServletConfiguration(ui = myUI.class, productionMode = true)
     public static class myUIServlet extends VaadinServlet {
     }
 
